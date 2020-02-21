88f542716c00a3a2175c2b798b468321e04e0075
alieu23 <54537888+alieu23@users.noreply.github.com>
2020-02-21 15:27:03 +0000

    update

--- flights.py
+++ flights.py
@@ -1,15 +1,24 @@
 import flightradar24
 import requests
 import pandas as pd
+from pandas import DataFrame
 
 
 def getflight():
+    air = []
     airline = 'BAW'
     fr = flightradar24.Api()
     flights = fr.get_flights(airline)
-    for key, value in flights.items():
+    data= list(flights.values())
+    x=2
+    while x>=2:
+        x=x+1
+        data1 = data[x][11:14]
+        air.append(data1)
+        df = pd.DataFrame(air)
+        df.to_csv('flights.csv', index=False)
+        print(data1)
 
-        print(key,value)
     return flights
 
 def airlines():
